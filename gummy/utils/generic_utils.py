71efdebc0dc2448919fdc2fa76c424afd5c64aec
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-06-30 05:25:57 +0900

    generic functions.

---
+++ gummy/utils/generic_utils.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+import os
+
+def recreate_dir(path, exist_ok=True):
+    if os.path.exists(path):
+        if exist_ok:
+            if os.path.isdir(path):
+                print(toRED("Delete existing directory"))
+                shutil.rmtree(path)
+            else:
+                print(toRED("Delete existing file."))
+                os.remove(path)
+        else:
+            raise FileExistsError(f"[Errno 17] File exists: '{path}'")
+    os.makedirs(path, exist_ok=False)
