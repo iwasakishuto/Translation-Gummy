75129f937e93ff70d36f1b1877d82ea08f654e8d
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-06-30 14:21:23 +0900

    Avoid setting up every time.

--- gummy/utils/driver_utils.py
+++ gummy/utils/driver_utils.py
@@ -2,44 +2,56 @@
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
-
 from kerasy.utils import toBLUE, toGREEN, toRED
 
-def print_log(is_succeed, pos):
-    if is_succeed:
-        flag = toGREEN("[success]")
-        content = "driver can be built."
-    else:
-        flag = toRED("[failure]")
-        content = "driver can't be built."
-    print(" ".join([flag, pos, content]))
+from .generic_utils import print_log
 
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--ignore-certificate-errors')
-# chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-dev-shm-usage')
+default_chrome_options = Options()
+default_chrome_options.add_argument('--no-sandbox')
+default_chrome_options.add_argument('--ignore-certificate-errors')
+default_chrome_options.add_argument('--headless')
+default_chrome_options.add_argument('--disable-dev-shm-usage')
 
-DRIVER_TYPE = "none"
+def check_driver(chrome_options=default_chrome_options):
+    DRIVER_TYPE = "none"
+    try:
+        with webdriver.Chrome(options=chrome_options) as driver:
+            DRIVER_TYPE = "local"
+            print_log(is_succeed=True, pos="local")
+    except:
+        print_log(is_succeed=False, pos="local")
+
+    try:
+        with webdriver.Remote(
+            command_executor='http://selenium:4444/wd/hub',
+            desired_capabilities=DesiredCapabilities.CHROME.copy(),
+            options=chrome_options) as driver:
+            DRIVER_TYPE = "remote"
+            print_log(is_succeed=True, pos="remote")
+    except:
+        print_log(is_succeed=False, pos="remote")
+    return DRIVER_TYPE
+
+############################
+#  START: Check driver
+############################
+
 try:
-    with webdriver.Chrome(options=chrome_options) as driver:
-        DRIVER_TYPE = "local"
-        print_log(is_succeed=True, pos="local")
-except:
-    print_log(is_succeed=False, pos="local")
+    __DRIVER_SETUP__
+except NameError:
+    DRIVER_TYPE = check_driver(chrome_options=default_chrome_options)
+    print(f"DRIVER_TYPE: {toGREEN(DRIVER_TYPE)}")
+    __DRIVER_SETUP__ = True
 
-try:
-    with webdriver.Remote(
-        command_executor='http://selenium:4444/wd/hub',
-        desired_capabilities=DesiredCapabilities.CHROME.copy(),
-        options=chrome_options) as driver:
-        DRIVER_TYPE = "remote"
-        print_log(is_succeed=True, pos="remote")
-except:
-    print_log(is_succeed=False, pos="remote")
-print(f"DRIVER_TYPE: {toGREEN(DRIVER_TYPE)}")
+############################
+#  END: Check driver
+############################
 
-def get_driver():
+
+def get_driver(chrome_options=None):
+    print(f"DRIVER_TYPE: {toGREEN(DRIVER_TYPE)}")
+    if chrome_options is None:
+        chrome_options = default_chrome_options
     if DRIVER_TYPE=="remote":
         driver = webdriver.Remote(command_executor='http://selenium:4444/wd/hub',
                                   desired_capabilities=DesiredCapabilities.CHROME.copy(),
