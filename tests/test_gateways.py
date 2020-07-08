1ce0d91908214af425d3efa806b05a1e79858509
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-07-09 03:54:51 +0900

    Ignore original Warnings and minor updated.

--- tests/test_gateways.py
+++ tests/test_gateways.py
@@ -9,15 +9,15 @@ def _test_gateways(gateway, url="https://doi.org/10.1038/171737a0", **kwargs):
     # Crawler without gateway.
     useless_crawler = journals.get(identifier=journal_type, gateway="useless")
     with get_driver() as driver:
-        useless_title, useless_texts = useless_crawler.get_contents(url=url, driver=driver)
+        useless_title, useless_contents = useless_crawler.get_contents(url=url, driver=driver)
 
     # Crawler with gateway.
     gateway_crawler = journals.get(identifier=journal_type, gateway=gateway)
     with get_driver() as driver:
-        gateway_title, gateway_texts = gateway_crawler.get_contents(url=url, driver=driver)
+        gateway_title, gateway_contents = gateway_crawler.get_contents(url=url, driver=driver)
         
     assert useless_title == gateway_title
-    assert len(useless_texts) < len(gateway_texts)
+    assert len(useless_contents) < len(gateway_contents)
 
 def test_utokyo_gateways():
     _test_gateways(gateway="utokyo")
