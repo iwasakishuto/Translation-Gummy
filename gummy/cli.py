64c7ad2db77b59640357dab50bc7387ed8dcf566
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-07-09 22:49:31 +0900

    Program for CLI.

--- gummy/cli.py
+++ gummy/cli.py
@@ -3,18 +3,9 @@ import sys
 import argparse
 from selenium.webdriver.chrome.options import Options
 
-from .utils._path import TEMPLATES_DIR
 from .models import TranslationGummy
-
-def _set_chrome_options(gui=False):
-    chrome_options = Options()
-    if not gui:
-        chrome_options.add_argument("--headless")
-    # When you run a crawler in docker, these options are important!!
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--ignore-certificate-errors")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    return chrome_options
+from .utils._path import TEMPLATES_DIR
+from .utils.driver_utils import get_chrome_options
 
 def translate_journal(argv=sys.argv[1:]):
     parser = argparse.ArgumentParser(prog="gummy-journal", add_help=True)
@@ -23,14 +14,14 @@ def translate_journal(argv=sys.argv[1:]):
     parser.add_argument("-T", "--translator", type=str, default="deepl",   help="Translator identifier, string name of a translator")
     parser.add_argument("-J", "--journal",    type=str, default=None,      help="Journal identifier, string name of a journal")
     # Chrome options
-    parser.add_argument("--gui", action="store_true", help="Whether you want to run Chrome with GUI browser.")
+    parser.add_argument("--browser", action="store_true", help="Whether you want to run Chrome with GUI browser.")
     # PDF format
     parser.add_argument("-pdf", "--pdf-path", type=str, default=None, help="Path to output pdf file path.")
     parser.add_argument("-tpl", "--tpl-path", type=str, default=None, help="Path to template path.")
     parser.add_argument("--delete-html", action="store_false", help="Whether you want to delete an intermediate product (html)")
     args = parser.parse_args(argv)
 
-    chrome_options = _set_chrome_options(args.gui)
+    chrome_options = get_chrome_options(browser=args.browser)
     url = args.url
     gateway = args.gateway
     translator = args.translator
@@ -49,7 +40,7 @@ def translate_journal(argv=sys.argv[1:]):
     model = TranslationGummy(chrome_options=chrome_options, gateway=gateway, translator=translator)
     pdf_path = model.toPDF(
         url=url, path=pdf_path, journal_type=journal_type, gateway=gateway,
-        searchpath=searchpath, template=template=, delete_html=delete_html, 
+        searchpath=searchpath, template=template, delete_html=delete_html, 
     )
     return pdf_path
 
@@ -57,11 +48,13 @@ def translate_text(argv=sys.argv[1:]):
     parser = argparse.ArgumentParser(prog="gummy-translate", add_help=True)
     parser.add_argument("query", type=str, help="English to be translated")
     parser.add_argument("-T", "--translator", type=str, default="deepl",   help="Translator identifier, string name of a translator")
-    parser.add_argument("--gui", action="store_true", help="Whether you want to run Chrome with GUI browser.")
+    parser.add_argument("--browser", action="store_true", help="Whether you want to run Chrome with GUI browser.")
     args = parser.parse_args(argv)
 
-    chrome_options = _set_chrome_options(args.gui)
+    chrome_options = get_chrome_options(browser=args.browser)
     query = args.query
+    translator = args.translator
+
     model = TranslationGummy(chrome_options=chrome_options, gateway="useless", translator=translator)
     japanese = model.en2ja(query=query)
     return japanese
