0f71822bc05f0ef2417e6160caa9224d9147e21c
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-07-10 01:40:03 +0900

    Use @pytest.mark.parametrize for parametrizing test functions.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,10 +1,14 @@
 # coding: utf-8
 import os
+import pytest
 from gummy.models import TranslationGummy
 from gummy import gateways
 from gummy import translators
 
-def _test_models(db, url, gateway="utokyo", translator="deepl"):
+@pytest.mark.parametrize("url", ["https://arxiv.org/abs/2003.03253", "https://doi.org/10.1038/171737a0"])
+@pytest.mark.parametrize("gateway", list(gateways.all.keys()))
+@pytest.mark.parametrize("translator", list(translators.all.keys()))
+def test_models(db, url, gateway, translator):
     gummy = TranslationGummy(gateway=gateway, translator=translator)
     
     # Make HTML & PDF.
@@ -12,9 +16,3 @@ def _test_models(db, url, gateway="utokyo", translator="deepl"):
     os.remove(htmlpath)
     pdfpath  = gummy.toPDF(url=url, delete_html=True)
     os.remove(pdfpath)
-
-def test_model_all(db):
-    for url in ["https://arxiv.org/abs/2003.03253", "https://doi.org/10.1038/171737a0"]:
-        for gateway in gateways.all.keys():
-            for translator in translators.all.keys():
-                _test_models(db=db, url=url, gateway=gateway, translator=translator)
