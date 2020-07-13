76ad2f7d370ec17893bc977376ac1a3020dd42eb
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-07-14 00:53:32 +0900

    Returned.

--- tests/test_journals.py
+++ tests/test_journals.py
@@ -4,12 +4,12 @@ from gummy.utils import get_driver
 from gummy import journals
 from data import JournalData
 
-# @pytest.mark.parametrize("journal_type", list(JournalData.keys()))
-# def test_journals(db, journal_type, gateway="useless", **kwargs):
-#     url = db.journals.get(journal_type)
-#     crawler = journals.get(identifier=journal_type, gateway=gateway)
-#     with get_driver() as driver:
-#         title, texts = crawler.get_contents(url=url, driver=driver)
+@pytest.mark.parametrize("journal_type", list(JournalData.keys()))
+def test_journals(db, journal_type, gateway="useless", **kwargs):
+    url = db.journals.get(journal_type)
+    crawler = journals.get(identifier=journal_type, gateway=gateway)
+    with get_driver() as driver:
+        title, texts = crawler.get_contents(url=url, driver=driver)
         
-#     assert len(title) > 0
-#     assert len(texts) > 0
+    assert len(title) > 0
+    assert len(texts) > 0
