32e1d673e61c4813cfe1b163516da965696a7313
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-11-26 17:32:11 +0900

    Version Updates (docker env & supported 3 journals)

--- gummy/__init__.py
+++ gummy/__init__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 __copyright__       = "Copyright (C) 2020 Shuto Iwasaki"
-__version__         = "3.5.2"
+__version__         = "3.6.0"
 
 __license__         = "MIT"
 __author__          = "Shuto Iwasaki"
