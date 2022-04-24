73ed0d0447c6c0b87c3873c676631f12550bbc1f
iwasakishuto <cabernet.rock@gmail.com>
2022-04-24 17:43:28 +0900

    :art: Minor Updates.

--- gummy/utils/templates.py
+++ gummy/utils/templates.py
@@ -3,9 +3,11 @@
 You can see the same contents by looking at `Github <https://github.com/iwasakishuto/Translation-Gummy/tree/master/gummy/templates>`_ .
 """
 import os
-from typing import Callable
+from typing import Callable, List
 
 from gummy.utils import TEMPLATES_DIR
+
+_ALLOWED_TEMPLATE_EXTENSIONS: List[str] = ["html", "htm", "css", "less"]
 
 
 def _mk_func(fn: str) -> Callable[[], str]:
@@ -31,7 +33,6 @@ def _mk_func(fn: str) -> Callable[[], str]:
 
 
 for fn in os.listdir(TEMPLATES_DIR):
-    if fn[-5:] != ".html":
-        continue
-    name = fn.replace(".", "_")
-    exec(f"{name} = _mk_func('{fn}')")
+    if any([fn.endswith(ext) for ext in _ALLOWED_TEMPLATE_EXTENSIONS]):
+        name: str = fn.replace(".", "_")
+        exec(f"{name} = _mk_func('{fn}')")
